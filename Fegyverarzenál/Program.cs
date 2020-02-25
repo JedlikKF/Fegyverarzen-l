d71b814b4a686de7607a1b91cec2f2591a0273f8
Kanizsa.Ferenc.Márk <Kanizsa.Ferenc.Mrk@C102-10>
2020-02-25 13:17:14 +0100

    fegyvertár lista létrehozva

--- Fegyverarzenál/Program.cs
+++ Fegyverarzenál/Program.cs
@@ -8,8 +8,18 @@ namespace Fegyverarzenál
 {
     class Program
     {
+             enum fegyverek {apaökle, uzi, vipera, AK47, rpg7,};
         static void Main(string[] args)
         {
+            fegyverek kezemben = fegyverek.apaökle;
+            List<fegyverek> fegyvertár = new List<fegyverek>();
+            fegyvertár.Add(fegyverek.uzi);
+            fegyvertár.Add(fegyverek.vipera);
+            foreach (fegyverek fegyver in fegyvertár)
+            {
+                Console.Write($"{fegyver} ");
+            }
+
             Console.ReadKey();
         }
     }
